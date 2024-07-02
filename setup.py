0f02abe25d60eeb9b05b137d9fe57cbf323a7b82
basil-hameed <basil@guvi.in>
2024-07-02 18:15:23 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="visual-comparison",
+    version="1.0.2.5",
+    description="Image Comparison Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BASILAHAMED/visual-comparison",
+    author="Basil Ahamed",
+    author_email="sbasil.ahamed@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="visual compare image diff testing",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        "numpy~=1.26.4",
+        "opencv-python~=4.9.0.80",
+        "scikit-image~=0.23.1"
+    ],
+    zip_safe=False,
+)
